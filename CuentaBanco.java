25e55528e2516e7e8524629c6a8d64aacbacf8fd
JulianDGM <jdgm701@gmail.com>
2025-02-22 12:43:20 -0500

    Add files via upload

--- CuentaBanco.java
+++ CuentaBanco.java
@@ -6,24 +6,33 @@ package ttclase3;
 import java.util.Scanner;
 
 public class CuentaBanco {
-   // Funciones:
-    public static double consultaSaldo(){
-        double saldo = 123897.4;
+   //Atributos clase
+    public static double totalCuenta=123897.4;
+
+// Funciones:
+    public static double consultaSaldo(double totalCuenta){
+        double saldo = totalCuenta;
         return saldo;
     }
     
-    public static double hacerRetiro(){
+    public static double hacerRetiro(double saldo){
         
         Scanner scanner = new Scanner(System.in);
             double valor = scanner.nextDouble();
+            System.out.println("¿Cuánto desear retirar?");
             scanner.nextLine();
-            //hacerlo nosotros
+            
+            if (saldo>=valor){
+                saldo -=valor;
+                System.out.println("Retiro exitoso, su nuevo saldo es: " +saldo);
+                } else {
+                System.out.println("Saldo insuficiente.");
+            }
             return valor;
     }
-        
-        
         //Método Main:
     public static void main(String[] args){
+       
         while(true){
             String Usuario = "Samuel Torres";
             System.out.println("Hola "+Usuario+"\n");
@@ -36,26 +45,35 @@ public class CuentaBanco {
             Scanner scanner = new Scanner(System.in);
             int opcion = scanner.nextInt();
             scanner.nextLine();
-            
+            double saldoActual = consultaSaldo(totalCuenta);
             if(opcion==4){
+                System.out.println("¡Gracias por usar nuestro sistema! Adiós.");
                 break;
-            }
-            
+            }            
             switch(opcion){
                 case 1:
-                    System.out.println("Consultar Saldo");
+                    System.out.println("\n---Consultar Saldo---");
+                    System.out.println("Su saldo actual es: "+ saldoActual+"\n1. Volver.\n2. Terminar.");
+                    int opcion_1 = scanner.nextInt();
+                    scanner.nextLine();
+                    if (opcion_1==2){
+                    System.out.println("¡Gracias por usar nuestro sistema! Adiós.");
                     break;
+                    }else if(opcion_1==1){
+                        break;
+                    }
                 case 2:
-                    System.out.println("Hacer Retiros");
+                    System.out.println("\n---Hacer Retiros---");
+                    System.out.println("¿Cuánto deseas retirar?");
+                    
+                        
                     break;
                 case 3:
                     System.out.println("Hacer Consignación");
                     break;
-                case 4:
-                    System.out.println("Salir");
-                    break;
-                    
-            }
+                default:
+                    System.out.println("La opción elegida no es valida");                 
+            }             
         }        
     }
     
